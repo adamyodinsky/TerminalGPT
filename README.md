68545296d05d706a49900f8f52c21484e138e401
Adam Yodinsky <27074934+adamyodinsky@users.noreply.github.com>
2023-03-02 19:04:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# TerminalGPT
+
+Welcome to the terminal-based ChatGPT personal assistant app! With the terminalGPT command, you can easily interact with ChatGPT and receive short, easy-to-read answers on your terminal.
+
+ChatGPT is specifically optimized for your machine's operating system, distribution, and chipset architecture, so you can be sure that the information and assistance you receive are tailored to your specific setup.
+
+Whether you need help with a quick question or want to explore a complex topic, TerminalGPT is here to assist you. Simply enter your query and TerminalGPT will provide you with the best answer possible based on its extensive knowledge base.
+
+Thank you for using TerminalGPT, and we hope you find the terminal-based app to be a valuable resource for your day-to-day needs!
+
+# Installation and Usage
+
+1. Install the package with pip install.
+
+```sh
+pip install terminalgpt -U
+```
+
+2. (Optional) Inject the token to the executable script on your local machine so you don't have to export it every time you open a new terminal
+
+```sh
+export OPENAI_API_KEY=<YOUR_OPEN_AI_KEY>
+./inject_token.sh
+```
+
+
+
+## Usage
+
+```sh
+terminalgpt
+```
--- terminalgpt/main.py
+++ terminalgpt/main.py
@@ -1,11 +1,17 @@
 import os
-from models import *
 from colorama import Fore, Back, Style
 import platform
+import tiktoken
+import openai
 
-total_usage = 0
 API_TOKEN_LIMIT = 4096
 LOCAL_TOKEN_LIMIT = API_TOKEN_LIMIT / 2
+
+MODEL = "gpt-3.5-turbo"
+ENCODING_MODEL = "gpt2"
+
+TIKTOKEN_ENCODER = tiktoken.get_encoding(ENCODING_MODEL)
+
 
 INIT_SYSTEM_MESSAGE = {
     "role": "system",
@@ -15,42 +21,58 @@ Please note that your answers will be displayed on the terminal.
 So keep them short as possible (7 new lines max) and use a suitable format for printing on terminal."""
 }
 
-messages = [
-        INIT_SYSTEM_MESSAGE,
-]
+def main():
+  total_usage = 0
+  messages = [
+          INIT_SYSTEM_MESSAGE,
+  ]
 
-while (True):
-    # Get user input
-    user_input=input(Style.BRIGHT + "\nUser: " + Style.RESET_ALL)
-    usage = num_tokens_from_string(user_input)
+  while (True):
+      # Get user input
+      user_input=input(Style.BRIGHT + "\nUser: " + Style.RESET_ALL)
+      usage = num_tokens_from_string(user_input)
 
-    while total_usage + usage >= LOCAL_TOKEN_LIMIT:
-        while total_usage + usage >= LOCAL_TOKEN_LIMIT / 2:
-          popped_message = messages.pop(0)
-          total_usage =- num_tokens_from_string(popped_message['content'])
-        
-        if total_usage + usage < LOCAL_TOKEN_LIMIT:
-            messages.insert(0, INIT_SYSTEM_MESSAGE)
-            
+      while total_usage + usage >= LOCAL_TOKEN_LIMIT:
+          while total_usage + usage >= LOCAL_TOKEN_LIMIT / 2:
+            popped_message = messages.pop(0)
+            total_usage =- num_tokens_from_string(popped_message['content'])
+          
+          if total_usage + usage < LOCAL_TOKEN_LIMIT:
+              messages.insert(0, INIT_SYSTEM_MESSAGE)
+              
 
-    # append to messages and send to ChatGPT
-    messages.append({"role": "user", "content": user_input})
+      # append to messages and send to ChatGPT
+      messages.append({"role": "user", "content": user_input})
 
-    # get answer
-    answer = get_answer(messages)
+      # get answer
+      answer = get_answer(messages)
 
-    # parse usage and message from answer
-    total_usage = answer['usage']['total_tokens']
-    message = answer['choices'][0]['message']['content']
+      # parse usage and message from answer
+      total_usage = answer['usage']['total_tokens']
+      message = answer['choices'][0]['message']['content']
 
-    # append to messages list for next iteration keeping context
-    messages.append({"role": "assistant", "content": message})
+      # append to messages list for next iteration keeping context
+      messages.append({"role": "assistant", "content": message})
 
-    # print answer message
-    print(Style.BRIGHT + "\nAssistant:" + Style.RESET_ALL)
-    print(Fore.YELLOW + message + Style.RESET_ALL)
-    
-    # print usage
-    if (os.getenv("DEBUG") == "True"):  
-      print(Back.LIGHTBLUE_EX + "Total Usage: " + str(total_usage) + " tokens" + Style.RESET_ALL)
-    
+      # print answer message
+      print(Style.BRIGHT + "\nAssistant:" + Style.RESET_ALL)
+      print(Fore.YELLOW + message + Style.RESET_ALL)
+      
+      # print usage
+      if (os.getenv("DEBUG") == "True"):  
+        print(Back.LIGHTBLUE_EX + "Total Usage: " + str(total_usage) + " tokens" + Style.RESET_ALL)
+      
+
+
+def num_tokens_from_string(string: str) -> int:
+    """Returns the number of tokens in a text string."""
+    num_tokens = len(TIKTOKEN_ENCODER.encode(string))
+    return num_tokens
+
+def get_answer(messages):
+    answer = openai.ChatCompletion.create(
+      model=MODEL,
+      messages=messages
+    )
+    return answer
+
