a70e814d7e999e592416e3e3cc817d77f7a10ba8
Adam Yodinsky <27074934+adamyodinsky@users.noreply.github.com>
2023-03-05 11:17:19 +0200

    fix terminating loop

--- terminalgpt/main.py
+++ terminalgpt/main.py
@@ -19,52 +19,65 @@ INIT_SYSTEM_MESSAGE = {
     "content": f"""
 You are a helpful terminal assistant for a programer on a {platform.platform()} machine.
 Please note that your answers will be displayed on the terminal.
-So keep them short as possible (7 new lines max) and use a suitable format for printing on terminal."""
+So keep them short as possible (7 new lines max) and use a suitable format for printing on terminal.""",
 }
 
+
 def main():
-  total_usage = 0
-  messages = [
-          INIT_SYSTEM_MESSAGE,
-  ]
+    try:
+        chat_loop()
+    except KeyboardInterrupt:
+        print(Style.BRIGHT + Fore.CYAN + "\n\nExiting..." + Style.RESET_ALL)
+        exit(0)
 
-  while (True):
-      # Get user input
-      user_input=input(Style.BRIGHT + "\nUser: " + Style.RESET_ALL)
-      usage = num_tokens_from_string(user_input)
 
-      # Prevent reaching tokens limit
-      while total_usage + usage >= LOCAL_TOKEN_LIMIT:
-          # When reaching the limit, remove half of the oldest messages from the context
-          while total_usage + usage >= LOCAL_TOKEN_LIMIT / 2:
-            popped_message = messages.pop(0)
-            total_usage -= num_tokens_from_string(popped_message['content'])
+def chat_loop():
+    total_usage = 0
+    messages = [
+        INIT_SYSTEM_MESSAGE,
+    ]
 
-          if total_usage + usage < LOCAL_TOKEN_LIMIT:
-              messages.insert(0, INIT_SYSTEM_MESSAGE)
-              
+    while True:
+        # Get user input
+        user_input = input(Style.BRIGHT + "\nUser: " + Style.RESET_ALL)
+        usage = num_tokens_from_string(user_input)
 
-      # Append to messages and send to ChatGPT
-      messages.append({"role": "user", "content": user_input})
+        # Prevent reaching tokens limit
+        while total_usage + usage >= LOCAL_TOKEN_LIMIT:
+            # When reaching the limit, remove half of the oldest messages from the context
+            while total_usage + usage >= LOCAL_TOKEN_LIMIT / 2:
+                popped_message = messages.pop(0)
+                total_usage -= num_tokens_from_string(popped_message["content"])
 
-      # Get answer
-      answer = get_answer(messages)
+            if total_usage + usage < LOCAL_TOKEN_LIMIT:
+                messages.insert(0, INIT_SYSTEM_MESSAGE)
 
-      # Parse usage and message from answer
-      total_usage = answer['usage']['total_tokens']
-      message = answer['choices'][0]['message']['content']
+        # Append to messages and send to ChatGPT
+        messages.append({"role": "user", "content": user_input})
 
-      # Append to messages list for next iteration keeping context
-      messages.append({"role": "assistant", "content": message})
+        # Get answer
+        answer = get_answer(messages)
 
-      # Print answer message
-      print(Style.BRIGHT + "\nAssistant:" + Style.RESET_ALL)
-      print_slowly(Fore.YELLOW + message + Style.RESET_ALL)
-      
-      # Print usage
-      if (os.getenv("DEBUG") == "True"):  
-        print(Back.LIGHTBLUE_EX + "Total Usage: " + str(total_usage) + " tokens" + Style.RESET_ALL)
-      
+        # Parse usage and message from answer
+        total_usage = answer["usage"]["total_tokens"]
+        message = answer["choices"][0]["message"]["content"]
+
+        # Append to messages list for next iteration keeping context
+        messages.append({"role": "assistant", "content": message})
+
+        # Print answer message
+        print(Style.BRIGHT + "\nAssistant:" + Style.RESET_ALL)
+        print_slowly(Fore.YELLOW + message + Style.RESET_ALL)
+
+        # Print usage
+        if os.getenv("DEBUG") == "True":
+            print(
+                Back.LIGHTBLUE_EX
+                + "Total Usage: "
+                + str(total_usage)
+                + " tokens"
+                + Style.RESET_ALL
+            )
 
 
 def num_tokens_from_string(string: str) -> int:
@@ -72,16 +85,14 @@ def num_tokens_from_string(string: str) -> int:
     num_tokens = len(TIKTOKEN_ENCODER.encode(string))
     return num_tokens
 
+
 def get_answer(messages):
-    answer = openai.ChatCompletion.create(
-      model=MODEL,
-      messages=messages
-    )
+    answer = openai.ChatCompletion.create(model=MODEL, messages=messages)
     return answer
+
 
 def print_slowly(text, delay=0.02):
     for char in text:
-        print(char, end='', flush=True)
+        print(char, end="", flush=True)
         time.sleep(delay)
     print()
-    
