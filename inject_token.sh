e318d6000df4155423a7027e0a150bcbee6a2849
Adam Yodinsky <27074934+adamyodinsky@users.noreply.github.com>
2023-03-06 10:58:31 +0200

    fix inject script

--- inject_token.sh
+++ inject_token.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-cat "$(which terminalgpt)" | sed '1d' > /tmp/terminalgpt
+cat "$(which terminalgpt)" | sed '1d' > /tmp/terminalgpt_exec_script
 echo "#!/usr/bin/env bash" > "$(which terminalgpt)"
 
 echo "export OPENAI_API_KEY=${OPENAI_API_KEY}" >> "$(which terminalgpt)"
-cat /tmp/terminalgpt >> "$(which terminalgpt)"
+cat /tmp/terminalgpt_exec_script >> "$(which terminalgpt)"
+
+rm /tmp/terminalgpt_exec_script
